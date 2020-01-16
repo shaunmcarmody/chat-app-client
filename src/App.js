2004a0311fc08a371ad2984fdd43a307f87bc532
shaunmcarmody <shaun.carmody@live.co.uk>
2020-01-16 18:36:08 +0000

    initialise applications state and component views

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { Portal, Splash } from './components/views';
 
 class App extends Component {
   state = {
-    
+    user: null,
+    messages: [],
+    error: ''
   }
+  
   render() {
     return (
       <div className="App">
+        {
+          this.state.user ?
+            <Portal
+            />
+            :
+            <Splash
+            />
+        }
       </div>
     );
   }
